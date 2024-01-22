b7c15cc2adb2af2745bfb111ea092226c63a43b6
toinou887 <rionde8u@etu.univ-lorraine.fr>
2024-01-22 17:12:45 +0100

    ajout de quelques tests dans PersonneTest et ProductTest

--- tests/Entity/PersonneTest.php
+++ tests/Entity/PersonneTest.php
@@ -9,28 +9,38 @@ use PHPUnit\Framework\TestCase;
 class PersonneTest extends TestCase
 {
 
-    public function testPersonne(): void
+    protected static $personne;
+    protected static $personne2;
+    protected static $personne3;
+
+    public static function setUpBeforeClass(): void
     {
-        $personne = new Person("John Doe", "EUR");
-        $this->assertEquals("John Doe", $personne->getName());
-        $this->assertEquals("EUR", $personne->getWallet()->getCurrency());
+        self::$personne = new Person("John Doe", "EUR");
+        self::$personne2 = new Person("Jane Doe", "EUR");
+        self::$personne3 = new Person("John Doe", "USD");
     }
 
-    public function testPersonne2(): void
+    public function testGetCurrencyPersonne(): void
     {
-        $personne = new Person("John Doe", "EUR");
-        $personne->setName("Jane Doe");
-        $this->assertEquals("Jane Doe", $personne->getName());
+        $this->assertEquals("John Doe", self::$personne->getName());
+        $this->assertEquals("EUR", self::$personne->getWallet()->getCurrency());
     }
 
-    public function testPersonne3(): void
+    public function testSetNamePersonne(): void
+    {
+//        $personne = new Person("John Doe", "EUR");
+        self::$personne->setName("Jane Doe");
+        $this->assertEquals("Jane Doe", self::$personne->getName());
+    }
+
+    public function testSetWalletPersonne(): void
     {
         $personne = new Person("John Doe", "EUR");
         $personne->setWallet(new Wallet("USD"));
         $this->assertEquals("USD", $personne->getWallet()->getCurrency());
     }
 
-    public function testPersonne4(): void
+    public function testAddFundPersonne(): void
     {
         $personne = new Person("John Doe", "EUR");
         $personne->setWallet(new Wallet("USD"));
@@ -38,7 +48,7 @@ class PersonneTest extends TestCase
         $this->assertEquals(100.0, $personne->getWallet()->getBalance());
     }
 
-    public function testPersonne5(): void
+    public function testRemoveFundPersonne(): void
     {
         $personne = new Person("John Doe", "EUR");
         $personne->setWallet(new Wallet("USD"));
--- tests/Entity/ProductTest.php
+++ tests/Entity/ProductTest.php
@@ -7,30 +7,65 @@ use PHPUnit\Framework\TestCase;
 
 class ProductTest extends TestCase
 {
-    public function testproduct1() : void {
-        $product = new Product("Product 1", [100], "food");
-        $this->assertEquals("Product 1", $product->getName());
-        $this->assertEquals(100.0, $product->getPrices()[0]);
-        $this->assertEquals("food", $product->getType());
+    protected static $product;
+    protected static $product2;
+    protected static $product3;
+    protected static $product4;
+    protected static $product5;
+    protected static $product6;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$product = new Product("Product 1", [["EUR"=>100], ["USD"=>200]], "food");
+        self::$product2 = new Product("Product 2", [["EUR"=>100], ["USD"=>200]], "food");
+        self::$product3 = new Product("Product 3", [["EUR"=>100], ["USD"=>200]], "food");
+        self::$product4 = new Product("Product 4", [["EUR"=>100], ["USD"=>200]], "food");
+        self::$product5 = new Product("Product 5", [["EUR"=>100], ["USD"=>200]], "food");
+        self::$product6 = new Product("Product 6", [["EUR"=>100], ["USD"=>200]], "food");
     }
 
-    public function testproduct2() : void {
-        $product = new Product("Product 2", [100, 200], "food");
+    public function testCreateProductNormal() : void {
+        $product = new Product("Product 2", [["EUR"=>100], ["USD"=>200]], "food");
         $this->assertEquals("Product 2", $product->getName());
         $this->assertEquals(100, $product->getPrices()[0]);
         $this->assertEquals(200, $product->getPrices()[1]);
         $this->assertEquals("food", $product->getType());
     }
 
-    // récupérer la tva d'un produit de type nourriture
-    public function testproduct3() : void {
-        $product = new Product("Product 3", [100, 200], "food");
+    public function testCreateProductSansPrix() : void {
+        $product = new Product("Product 2", [["EUR"], ["USD"]], "food");
+        $this->assertEquals("Product 2", $product->getName());
+        $this->assertEquals([], $product->getPrices());
+        $this->assertEquals("food", $product->getType());
+    }
+
+    public function testCreateProductSansType() : void {
+        $product = new Product("Product 2", [["EUR"=>100], ["USD"=>200]]);
+        $this->assertEquals("Product 2", $product->getName());
+        $this->assertEquals(100, $product->getPrices()[0]);
+        $this->assertEquals(200, $product->getPrices()[1]);
+        $this->assertEquals("other", $product->getType());
+    }
+
+    public function testGetTVAFood() : void {
+        $product = new Product("Product 3", [["EUR"=>100], ["USD"=>200]], "food");
         $this->assertEquals(0.1, $product->getTVA());
     }
 
-    // récupérer la tva d'un produit d'un autre type
-    public function testproduct4() : void {
-        $product = new Product("Product 4", [100, 200], "tech");
+    public function testGetTVAOtherType() : void {
+        $product = new Product("Product 4", [["EUR"=>100], ["USD"=>200]], "tech");
         $this->assertEquals(0.2, $product->getTVA());
+    }
+
+    // méthode pour tester setPrice
+    public function testSetPrices() : void {
+        $product = new Product("Product 5", [["EUR"=>100], ["USD"=>200]], "tech");
+        $product->setPrices([["EUR"=>200], ["USD"=>300]]);
+        $this->assertEquals(300, $product->getPrices()[0]);
+    }
+
+    public function testGetPrice() : void {
+        $product = new Product("Product 6", [["EUR"=>100], ["USD"=>200]], "tech");
+        $this->assertEquals(100.0, $product->getPrice("EUR"));
     }
 }
